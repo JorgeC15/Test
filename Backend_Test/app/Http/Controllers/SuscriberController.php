880db91503392e4e85f4f8ef5139389f51d10c8e
SEP <jorgecanche19@gmail.com>
2025-07-21 13:51:24 -0600

    Actualizando Api Backend

--- Backend_Test/app/Http/Controllers/SuscriberController.php
+++ Backend_Test/app/Http/Controllers/SuscriberController.php
@@ -12,41 +12,44 @@ class SuscriberController extends Controller
     {
         return Suscriber::all();
     }
-    public function create()
-    {
-        // Logic to show a form for creating a new subscriber
-    }   
+
     public function store(Request $request)
     {
-        // Logic to store subscriber data
-        // For example, you might validate and save the data to the database
-
         $request->validate([
             'name' => 'required|string|max:150',
             'email' => 'required|email|unique:suscribers,email',
         ]);
         $suscriber = Suscriber::create($request->all());
-        return response()->json($suscriber, 201);
+        return response()->json($suscriber, 200);
     }
+
     public function show($id)
     {
-        // Logic to show a specific subscriber
-        return view('suscriber.show', compact('id'));
+        $suscriber = Suscriber::findOrFail($id);
+        if (!$suscriber) {
+            return response()->json(['error' => 'Suscriber not found'], 404);
+        }
+        return response()->json($suscriber, 200);
     }
-    public function edit($id)
-    {
-        // Logic to edit a specific subscriber
-        
-    }
+
     public function update(Request $request, $id)
     {
-        // Logic to update subscriber data
-        // For example, you might validate and update the data in the database
-        
-    } 
+        $request -> validate([
+            'name' => 'sometimes|required|string|max:150',
+            'email' => 'sometimes|required|email|unique:suscribers,email,' . $id,
+        ]);
+        $suscriber = Suscriber::findOrFail($id);
+        $suscriber->update($request->all());
+        return response()->json($suscriber, 200);
+    }
+
     public function destroy($id)
     {
-        // Logic to delete a specific subscriber
-        
+        $suscriber = Suscriber::findOrFail($id);
+        if (!$suscriber) {
+            return response()->json(['error' => 'Suscriber not found'], 404);
+        }
+        $suscriber->delete();
+        return response()->json(null, 200);
     }   
 }
--- Backend_Test/routes/api.php
+++ Backend_Test/routes/api.php
@@ -4,12 +4,18 @@ use App\Http\Controllers\SuscriberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+//------------Rutas de la API para suscriptores------------//
 //Index -> mostrar subscribers
 Route::get('suscribers',[SuscriberController::class, 'index']);
-
+//show -> Mostrar un suscriptor
+Route::get('suscribers/{id}',[SuscriberController::class, 'show']);
 //store -> Almacenar suscriptor
 Route::post('suscribers',[SuscriberController::class, 'store']);
+//update -> Actualizar un suscriptor
+Route::put('suscribers/{id}',[SuscriberController::class, 'update']);
+//destroy -> Eliminar un suscriptor
+Route::delete('suscribers/{id}',[SuscriberController::class, 'destroy']);
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
